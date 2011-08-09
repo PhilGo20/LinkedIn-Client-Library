7c1b624708a0f6aee5640f108ea2fa89b9085c27
Michael Mulley <michael@michaelmulley.com>
2011-08-09 14:11:57 -0400

    Have get_user_connections accept a params dict instead of kwargs, in order to use URL parameters that aren't valid variable names (e.g. modified-since)

--- liclient/__init__.py
+++ liclient/__init__.py
@@ -83,7 +83,7 @@ class LinkedInAPI(object):
         
         return LinkedInXMLParser(content).results
     
-    def get_user_connections(self, access_token, selectors=None, **kwargs):
+    def get_user_connections(self, access_token, selectors=None, params=None):
         """
         Get the connections of the current user.  Valid keyword arguments are
         "count" and "start" for the number of profiles you wish returned.  Types
@@ -92,7 +92,9 @@ class LinkedInAPI(object):
         """
         if selectors:
             url = self.prepare_field_selectors(selectors, self.api_profile_connections_url)
-        user_token, url = self.prepare_request(access_token, url, kwargs)
+        if params is None:
+            params = {}
+        user_token, url = self.prepare_request(access_token, url, params)
         client = oauth.Client(self.consumer, user_token)
         resp, content = client.request(url, 'GET')
         return LinkedInXMLParser(content).results
